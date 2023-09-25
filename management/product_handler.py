e90f5e6426f979507d0f22895d2df7b9be96f5fc
unknown <helissonherold@yahoo.com.br>
2023-09-25 19:35:18 -0300

    feat: tarefa_3_finalizada

--- management/product_handler.py
+++ management/product_handler.py
@@ -1,7 +1,10 @@
 from menu import products
+from collections import Counter
 
 
 def get_product_by_id(id):
+    if type(id) != int:
+        raise TypeError(f'product id must be an int')
     for product in products:
         if product['_id'] == id:
             return product
@@ -10,6 +13,8 @@ def get_product_by_id(id):
 
 def get_products_by_type(kind):
     product_type = []
+    if type(kind) != str:
+        raise TypeError(f'product type must be a str')
     for iten in products:
         if iten['type'] == kind:
             product_type.append(iten)
@@ -27,3 +32,41 @@ def add_product(menu, **kwargs):
 
     menu.append(new_product)
     return new_product
+
+
+def menu_report():
+    product_count = len(products)
+    total_prices = []
+    total_types = []
+    for product in products:
+        total_prices.append(product['price'])
+        total_types.append(product['type'])
+
+    average_price = sum(total_prices)/product_count
+    counter_types = Counter(total_types)
+    common_type = counter_types.most_common(1)[0][0]
+
+    return f'Products Count: {product_count} - Average Price: ${round(average_price, 2)} - Most Common Type: {common_type}'
+
+
+def add_product_extra(menu, *required_keys, **product):
+    new_id = 1
+    product_keys = [*product]
+
+    for key in required_keys:
+        if key not in product_keys:
+            raise KeyError(f'field {key} is required')
+
+    for key in product_keys:
+        if key not in required_keys:
+            product.pop(key)
+
+    if len(menu) > 0:
+        new_id = max(product['_id'] for product in menu)+1
+    else:
+        new_id = 1
+
+    product['_id'] = new_id
+
+    menu.append(product)
+    return product
--- management/tab_handler.py
+++ management/tab_handler.py
@@ -2,9 +2,21 @@ from menu import products
 
 
 def calculate_tab(menu):
-    amount = 0 
+    amount = 0
     for object in menu:
         for product in products:
             if product['_id'] == object['_id']:
-                amount += product['price']*object['amount']               
+                amount += product['price']*object['amount']        
     return {'subtotal': f'${round(amount, 2)}'}
+
+
+def add_product(menu, **kwargs):
+    new_id = 1
+
+    if len(menu) > 0:
+        new_id = max(product['_id'] for product in menu)+1
+
+    new_product = {**kwargs, '_id': new_id}
+
+    menu.append(new_product)
+    return new_product
